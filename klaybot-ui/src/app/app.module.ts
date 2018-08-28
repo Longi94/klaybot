e5c490bbed8231d18d907c502f4a2907afc263ad
Long Tran <lngtrn94@gmail.com>
2018-08-28 16:19:49 +0200

    routing

--- klaybot-ui/src/app/app.module.ts
+++ klaybot-ui/src/app/app.module.ts
@@ -4,7 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "./shared/shared.module";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+import { StatsComponent } from "./front/stats/stats.component";
+import { HistoryComponent } from "./front/history/history.component";
+import { FrontModule } from "./front/front.module";
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'stats',
+        pathMatch: 'full'
+    },
+    {
+        path: 'stats',
+        component: StatsComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'history',
+        component: HistoryComponent,
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
     declarations: [
@@ -13,8 +34,9 @@ import { RouterModule } from "@angular/router";
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        RouterModule.forRoot(routes),
         SharedModule,
-        RouterModule
+        FrontModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- klaybot-ui/src/app/shared/shared.module.ts
+++ klaybot-ui/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { MatToolbarModule } from "@angular/material";
+import { MatButtonModule, MatToolbarModule } from "@angular/material";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule,
+        MatButtonModule,
         MatToolbarModule
     ],
     declarations: [ToolbarComponent],
