3be578a047b939536a2b7e8c8094562f9bd9a5d5
Long Tran <lngtrn94@gmail.com>
2018-09-29 18:04:44 +0200

    add delay to dadjoke

--- src/main/java/in/dragonbra/klayb0t/chat/DadJokeHandler.java
+++ src/main/java/in/dragonbra/klayb0t/chat/DadJokeHandler.java
@@ -14,13 +14,25 @@ import java.util.regex.Pattern;
 @Component
 public class DadJokeHandler extends MessageHandler {
 
+    private static final long MIN_DELAY = 60000L;
+
     private static final Pattern PATTERN = Pattern.compile("^(([Ii](\\sa|')?)|a)m\\s(?<target>.+)$");
 
     @Value("${twitch.bot.name}")
     private String botName;
 
+    private long lastTimestamp = 0L;
+
     @Override
-    public String handle(User user, String message) throws Exception {
+    public String handle(User user, String message) {
+        long currentTimestamp = System.currentTimeMillis();
+
+        if (lastTimestamp > currentTimestamp - MIN_DELAY) {
+            return null;
+        }
+
+        lastTimestamp = currentTimestamp;
+
         Matcher matcher = PATTERN.matcher(message);
 
         if (!matcher.matches()) {
