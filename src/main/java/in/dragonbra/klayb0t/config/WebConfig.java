c61091e9bb66f4ad8062263bc3efd47a530aaf18
Long Tran <lngtrn94@gmail.com>
2018-10-08 22:29:08 +0200

    fix cors

--- src/main/java/in/dragonbra/klayb0t/config/WebConfig.java
+++ src/main/java/in/dragonbra/klayb0t/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://klayyy.com");
             }
         };
     }
