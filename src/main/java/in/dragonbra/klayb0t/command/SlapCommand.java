5219803063f6f1d65b664d18f3b24a341fd4ce2e
Long Tran <lngtrn94@gmail.com>
2020-08-28 21:54:38 +0200

    add things

--- src/main/java/in/dragonbra/klayb0t/command/SlapCommand.java
+++ src/main/java/in/dragonbra/klayb0t/command/SlapCommand.java
@@ -1,5 +1,6 @@
 package in.dragonbra.klayb0t.command;
 
+import com.google.common.collect.ImmutableList;
 import org.pircbotx.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,11 @@ import java.util.Random;
 public class SlapCommand extends Command {
 
     private final Random rand = new Random();
+
+    private static final List<String> THINGS = ImmutableList.of(
+            "a cactus", "a trout", "a gun", "a book", "a pie", "their face", "their nipple", "spaghetti",
+            "big meaty claws", "adr525"
+    );
 
     @Value("${twitch.bot.name}")
     private String botName;
@@ -26,11 +32,12 @@ public class SlapCommand extends Command {
         users.remove(botName);
 
         String target = users.get(rand.nextInt(users.size()));
+        String item = THINGS.get(rand.nextInt(THINGS.size()));
 
         if (target.equals(user.getNick())) {
-            return String.format("%s slapped themselves, stop hitting yourself!", target);
+            return String.format("%s slapped themselves with %s, stop hitting yourself!", target, item);
         } else {
-            return String.format("%s slapped %s!", user.getNick(), target);
+            return String.format("%s slapped %s with %s!", user.getNick(), target, item);
         }
     }
 }
